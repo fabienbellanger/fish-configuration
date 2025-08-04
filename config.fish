b2a04ada42055f9a9e8199e2257ddd5f19b9416f
Fabien <fabienbellanger@users.noreply.github.com>
2025-08-04 14:22:41 +0200

    Create config.fish

---
+++ config.fish
@@ -0,0 +1,14 @@
+# Auto-Warpify
+status --is-interactive
+
+# Rust Cargo
+set -gx PATH ~/.cargo/bin $PATH
+
+# Flutter
+set -gx PATH ~/flutter/bin $PATH
+
+# Bun
+set -gx PATH ~/.bun/bin $PATH
+
+# LLVM
+set -gx PATH /usr/local/opt/llvm/bin $PATH
