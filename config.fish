d61235c3fc3d659f3d7b256182f24c3be876b532
Fabien <fabienbellanger@users.noreply.github.com>
2025-08-22 14:55:13 +0200

    Add NVM

--- config.fish
+++ config.fish
@@ -12,3 +12,10 @@ set -gx PATH ~/.bun/bin $PATH
 
 # LLVM
 set -gx PATH /usr/local/opt/llvm/bin $PATH
+
+# NVM
+set -gx NVM_DIR $HOME/.nvm
+mkdir -p $NVM_DIR
+function nvm
+    bass source (brew --prefix nvm)/nvm.sh ';' nvm $argv
+end
